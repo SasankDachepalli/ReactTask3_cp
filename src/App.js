322707d951745ac903129b7184f78c9a1de496df
SasankDachepalli <141177246+SasankDachepalli@users.noreply.github.com>
2023-11-10 13:05:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Colors from './Components/Colors';
+
+function App() {
+  return (
+    <div>
+      <Colors />
+    </div>
+  );
+}
+
+export default App;
